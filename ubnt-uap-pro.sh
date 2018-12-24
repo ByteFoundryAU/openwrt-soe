8c0cdcf25bb6c5527358163853fb1422cabedf03
Dean Hamstead <dean@fragfest.com.au>
2018-12-25 00:21:25 +1100

    Add tar, so that we have a proper tar command for backups

--- ubnt-uap-pro.sh
+++ ubnt-uap-pro.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 make clean
-mkdir -p files/etc/config
+mkdir -p files/etc/{dropbear,config}
 
 make image \
-    FILES=files/ \
     PROFILE=ubnt-uap-pro \
-    PACKAGES="-dnsmasq -ppp -ppp-mod-pppoe -kmod-ppp* ethtool lldpd wireless-tools -kmod-usb-core -kmod-usb-ohci -kmod-usb2 haveged snmpd"
+    FILES=files/ \
+    PACKAGES="-dnsmasq -ppp -ppp-mod-pppoe -kmod-ppp* ethtool lldpd wireless-tools -kmod-usb-core -kmod-usb-ohci -kmod-usb2 haveged snmpd tar"
 
