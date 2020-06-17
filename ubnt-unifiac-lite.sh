9ee56ba2de3f194c7930da520de0a65e8223cbad
Dean Hamstead <dean@fragfest.com.au>
2020-06-16 23:49:49 -0700

    Updated for new 19.07 and added some stuff

--- ubnt-unifiac-lite.sh
+++ ubnt-unifiac-lite.sh
@@ -4,12 +4,13 @@ make clean
 mkdir -p files/etc/{dropbear,config}
 
 make image \
-    PROFILE=ubnt-unifiac-lite \
+    PROFILE=ubnt_unifiac-lite \
     FILES=files/ \
     PACKAGES="-dnsmasq -ppp -ppp-mod-pppoe -kmod-ppp*  \
         -kmod-usb-core -kmod-usb-ohci -kmod-usb2 \
         ethtool lldpd wireless-tools \
-        haveged snmpd hostapd-utils \
+        haveged snmpd hostapd-utils iperf3 \
+        collectd collectd-mod-conntrack collectd-mod-contextswitch collectd-mod-cpu collectd-mod-df collectd-mod-entropy collectd-mod-ethstat collectd-mod-interface collectd-mod-irq collectd-mod-iwinfo collectd-mod-load collectd-mod-memory collectd-mod-network collectd-mod-processes collectd-mod-uptime collectd-mod-wireless \
         "
 #       tar"  # add tar if backing up with backuppc
 #       luci-ssl"   # add luci-ssl if a ui is needed
